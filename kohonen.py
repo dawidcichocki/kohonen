47fb4b936fff67bde7ebcb90fc974c090f2dc217
Dawid Cichocki <30655857+FailbaddonTheHarmless@users.noreply.github.com>
2019-12-10 22:59:11 +0100

    Add files via upload

--- kohonen.py
+++ kohonen.py
@@ -4,22 +4,24 @@ import matplotlib.pyplot as plt
 
 class kohonen(object):
     
-    def __init__(self, input_lenght, size_x = 25, size_y = 10,  learning_rate = 0.1 , neighbour_radious = 4, neighbour_function = 1):
+    def __init__(self, input_lenght, size_x = 25, size_y = 10,  learning_rate = 0.1 , neighbour_radious = 4, neighbour_function_type = 1):
         self.size_x = size_x
         self.size_y = size_y
         self.learning_rate = learning_rate
         self.neighbour_radious = neighbour_radious
-        self.neighbour_function = neighbour_function
+        self.neighbour_function_type = neighbour_function_type
         self.input_lenght = input_lenght
         # initializes net with size x,y and random initial values from range [0.25,0.5)
         self.neurons = ((np.random.random((size_x, size_y, input_lenght)) + 3) /4) -0.5
     
-        self.__distribuation = self.gaussian_distribuation()
+        self.__distribution = self.gaussian_distribution()
         
         self.eta = self.learning_rate
         self.r = self.neighbour_radious
         
-    def gaussian_distribuation(self, mean = 0, variance = 1):
+        self.u_matrix = None
+        
+    def gaussian_distribution(self, mean = 0, variance = 1):
         sigma = math.sqrt(variance)
         x=np.linspace(mean + 3 * sigma, 100)
         return x
@@ -36,10 +38,10 @@ class kohonen(object):
     
     def n_fun(self, xi, yi, xw, yw, r):
         #gaussian function
-        if self.neighbour_function:
+        if self.neighbour_function_type:
             d = self.metric(xi,yi,xw,yw)
             if d <= r:
-                return self.__distribuation[math.floor((d/r)*49)]
+                return self.__distribution[math.floor((d/r)*49)]
             else:
                 return 0
         
@@ -70,15 +72,35 @@ class kohonen(object):
         for ivector in input_vector:
             x, y = self.winner(ivector)
             self.neurons += eta * self.n_matrix(x, y, r)[:, :, np.newaxis] * (self.neurons - ivector)
-       self.decay()
+        self.decay()
         
     #todo: exp time decay
     def decay(self):
-        self.eta -= self.eta/300
-        self.r -= self.r/20
-        
+        self.eta -= self.eta/10
+        self.r -= self.r/2
+      
     def reference_neurons(self, positive, negative):
         return self.winner(positive), self.winner(negative)
+    
+    #needs optimization 
+    def u_matrix_calculation(self):
+        u_matrix = np.zeros((2*self.size_x -1, 2*self.seize_y - 1))
+        for x in range(self.size_x - 1):
+            for y in range(self.size_y - 1 ):
+                u_matrix[2*x, 2*y] = self.neurons[x,y]
+                u_matrix[2*x +1, 2*y] = self.euclidean(self.neurons[x,y], self.neurons[x+1,y])
+                u_matrix[2*x, 2*y +1] = self.euclidean(self.neurons[x,y], self.neurons[x,y+1])
+                u_matrix[2*x +1, 2*y +1] = 0,5*(u_matrix[2*x +1, 2*y] + u_matrix[2*x, 2*y +1])/np.sqrt(2)
+        u_matrix[2*self.size_x - 2, 2*self.size_y - 2] = self.neurons[self.size_x - 1, self.size_y - 1]
+        self.u_matrix = u_matrix
+        
+    def get_clusters(self):
+        #get watershed markers with opencv
+        #get boundries
+        #set clusters within boundries
+        #return clusters
+        pass             
+        
     
     def heat_map(self, vector):
         z = self.weight_metric(vector)
